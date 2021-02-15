69ee65047d7d7ef760e22d2f31fa9e7e0bdf14bd
Strle <ivan.starcevic15@gmail.com>
2021-02-15 05:41:21 +0100

    dodan brojač proizvoda

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#add");
 const  btn = document.querySelector("#btn");
 const list = document.querySelector("#list");
 
-// dodavanje novih proizvoda gumb Dodaj
+// dodavanje novih proizvoda, gumb Dodaj
 btn.onclick = function(){
 
     let txt = input.value;
@@ -14,17 +14,28 @@ btn.onclick = function(){
         li.innerHTML = txt;
         list.insertBefore(li, list.childNodes[0]);
         input.value = '';
+        countItems()
     }
-
 };
 
-// brisanje svih unešenih proizvoda gumb Obriši
+// brisanje svih unešenih proizvoda, gumb Obriši
 function deleteAll(){
-    var todo =  document.getElementById("list");
-    var lis = todo.getElementsByTagName("li");
+    let todo =  document.getElementById("list");
+    let lis = todo.getElementsByTagName("li");
     console.log(lis);
     while(lis.length > 0){
         todo.removeChild(lis[0]);
     }
-};
+    countItems()
+}
+
+// izračun i prikaz broja proizvoda u listi
+function countItems(){
+    let ul = document.getElementById('list');
+    let i=0, itemCount =0;
+
+    while(ul.getElementsByTagName('li') [i++]) itemCount++;
+    document.getElementById("myText").innerHTML = itemCount.toString();
+
+}
 
