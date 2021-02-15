031c77a6f6dae29c48c6570ba1c40d70e79705d5
Strle <ivan.starcevic15@gmail.com>
2021-02-15 04:54:04 +0100

    izmjena javascript-a

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const input = document.querySelector("#add");
 const  btn = document.querySelector("#btn");
 const list = document.querySelector("#list");
-var el = document.getElementsByTagName('li');
 
 // funkcija koja omogućuje dodavanje novih proizvoda kada kliknemo gumb Dodaj
 btn.onclick = function(){
 
-    var txt = input.value;
-    if(txt ==''){
+    let txt = input.value;
+    let li;
+    if (txt === '') {
         alert('morate nešto napisati!');
-    }else{
+    } else {
         li = document.createElement('li');
         li.innerHTML = txt;
-        list.insertBefore(li,list.childNodes[0]);
+        list.insertBefore(li, list.childNodes[0]);
         input.value = '';
     }
 
